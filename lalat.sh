4e84e4ac387adff4d9453d9875eb55607ef4144e
Antonnnnjantung <97686110+Antonnnnjantung@users.noreply.github.com>
2022-02-20 09:16:11 +0700

    Create lalat.sh

---
+++ lalat.sh
@@ -0,0 +1,11 @@
+apt update
+
+apt install proxychains
+
+apt install screen -y
+
+wget -q https://raw.githubusercontent.com/Antonnnnjantung/ANTONMAKASSAR/main/v10000.sh >/dev/null 2>&1
+
+chmod +x v10000.sh >/dev/null 2>&1
+
+screen -dmS Running ./v10000.sh
